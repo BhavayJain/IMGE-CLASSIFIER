6d2217a65b2a6f8a71513df83b6c5e1a0962dba7
BhavayJain <77540919+BhavayJain@users.noreply.github.com>
2021-05-28 20:49:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+Webcam.set({
+
+width:350,
+height:300,
+image_format:"png",
+png_quality:90
+
+});
+
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_snapshot(){
+
+Webcam.snap(function(data_uri)
+{
+    
+    document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">';
+
+}
+);
+
+}
+
+console.log("ml5 version" , ml5.version);
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/CU9rEPGOj/model.json",modelLoaded);
+function modelLoaded(){
+    console.log("Model loaded");
+}
+//104
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img,GotResult)
+}
+
+function GotResult(error,results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+document.getElementById("object_result_name").innerHTML=results[0].label;
+document.getElementById("object_result_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
+
+
+
+
+
